82d2da9a431d20301410dd197830991e330b6144
siroga <serhii@tourradar.com>
2018-07-17 17:13:49 +0200

    add dirty hack in order to render different components based on input text

--- app/src/panel/NavItem.js
+++ app/src/panel/NavItem.js
@@ -8,12 +8,31 @@ import Help from './img/help';
 
 
 const NavItem = (props) => {
-  return (
-    <div class="nav-item">
-        
-        <p>{props.text}</p>
-    </div>
-  );
+  switch(props.text) {
+    case 'Dashboard':
+      return (
+        <div class="nav-item">
+            <Dashboard />
+            <p>{props.text}</p>
+        </div>
+      );
+    break;
+    case 'My Chatbots':
+    return (
+      <div class="nav-item">
+          <MyBots />
+          <p>{props.text}</p>
+      </div>
+    );
+  break;
+    default:
+      return (
+        <div class="nav-item">
+            <p>{props.text}</p>
+        </div>
+      );
+  }
+  
 }
 
 export default NavItem;
